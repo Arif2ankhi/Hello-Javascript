ee4fca6ed6966f70cd99caeefdbf621d2276c9b0
Arif2ankhi <arif2ankhi@gmail.com>
2025-11-03 07:57:32 +0000

    second commit

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-let myName  = 'Arif';
-console.log(myName);
+
+
+// let num = 10;
+// console.log(typeof num);
+// let fullName = 'Munna';
+// console.log(typeof typeof fullName);
+
+// let isProgrammer = 'true';
+
+// console.log(typeof isProgrammer);
+
+let und = undefined;
+
+let none = null;
+
+console.log(typeof und);
+console.log(typeof none );
