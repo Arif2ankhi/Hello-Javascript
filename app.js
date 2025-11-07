4180cfca80af9114a41287e837eaf86b1d278e4c
Arif2ankhi <arif2ankhi@gmail.com>
2025-11-07 07:59:42 +0000

    seconds commit

--- app.js
+++ app.js
@@ -9,9 +9,69 @@
 
 // console.log(typeof isProgrammer);
 
-let und = undefined;
+// let und = undefined;
 
-let none = null;
+// let none = null;
 
-console.log(typeof und);
-console.log(typeof none );
+// console.log(typeof und);
+// console.log(typeof none );
+
+// let arr = { a : 1 };
+// console.log(typeof arr);
+
+// let num1 = 22456; 
+// let num2 = 9; 
+// // let num2 = '10'; 
+
+// console.log(num1 % 1000);
+
+
+// let age = 10;
+// let add = 'dhaka';
+// console.log(age > 9 ||  add == 'dhaka' );
+
+// let arr  = [ 1, 2, 3, 4, 5 ]
+
+// console.log(arr[0]);
+//  let num = 10;
+//   let newNum = num;
+
+//    num = 20; 
+
+//    console.log(num);
+
+//    let arr = ['topu', 'apu', 'montu'];
+//    arr[7] = 'rony';
+
+//    console.log(arr[3]);
+
+// //    let newArr = arr;
+
+// //    arr[2] = 50; 
+
+// //    console.log(newArr);
+//  arr =  ['habu', ...arr]
+//  arr.shift('popi');
+
+// arr[arr.length] = 'topu';
+// arr[arr.length]  = 'apu';
+// arr[arr.length]  = 'dopu';
+// arr[arr.length] = 'mopu';
+// arr[arr.length]  = 'zopu';
+
+// let newVal = arr.toString();
+
+// console.log(newVal);
+
+
+// let str = 'hello world';
+// let newArr = str.slice('');
+// console.log(newArr);
+
+let arr = [ 
+    ['topu', 'apu', 'montu'],
+    ['jopu', 'mpu', 'bilontu'],
+    ['topi', 'apui', 'montui']
+    ];
+
+    console.log(arr[2][2]);
